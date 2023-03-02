3034f7de23483c9556c164660796da8ce5500a69
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2023-03-02 12:01:49 +0530

    Delete courserbundlerserver/config directory

--- courserbundlerserver/config/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.set("strictQuery", false);
-export const connectDB = async () => {
-  const { connection } = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected with ${connection.host}`);
-};
